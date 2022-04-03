9771507f11739d7278dd9f081be6d2e5ce30dee4
pavel shevrin <77666606+Shevrin@users.noreply.github.com>
2022-04-03 15:34:22 +0300

    clear console.log, clear app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CheckViewsizeService } from './services/check-viewsize.service';
 import { CookiesService } from './services/cookies.service';
 import { NotifierService } from './services/notifier.service';
@@ -6,9 +6,8 @@ import { NotifierService } from './services/notifier.service';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements OnInit {
   constructor(
     private notifier: NotifierService,
     private checkViewSize: CheckViewsizeService,
@@ -19,12 +18,5 @@ export class AppComponent implements OnInit, OnDestroy {
     if (this.checkViewSize.checkView() && !this.cookies.getItem()) {
       this.notifier.openNotifier();
     }
-    console.log('init');
-  }
-
-  ngOnDestroy(): void {
-    console.log('destroy');
-
-    this.cookies.clearItem();
   }
 }
